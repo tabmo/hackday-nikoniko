dd583e3b25da799bd0819d86b4cc50d8b405ae11
Audrey Novak <audrey.novak@hawk-platform.io>
2018-03-16 16:38:48 +0100

    add trends command

--- scripts/nikoniko.js
+++ scripts/nikoniko.js
@@ -6,9 +6,11 @@
  #   niko test - affiche Bonjour !
  #   inscrit moi - Pour recevoir la question quotidienne `Comment s'est passé cette journée ?`
  #   désinscrit moi - Pour ne plus recevoir la question quotidienne `Comment s'est passé cette journée ?`
+ #   trends - Affiche la tendance de l'humeur
  */
 
 var moodQuestionFile = require('./moodMessageQuestion.json')
+const Spearman = require('spearman-rho')
 
 module.exports = function(robot) {
 
@@ -60,7 +62,7 @@ module.exports = function(robot) {
         .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
         .header('Accept', 'application/json')
         .get()
- 
+
     response(function(err, response, body) {
       records = JSON.parse(body).records[0]
       total = records.fields["1"] + records.fields["2"] + records.fields["3"] + records.fields["4"] + records.fields["5"]
@@ -114,4 +116,78 @@ module.exports = function(robot) {
       }
     })
   });
+
+
+
+  var sortDates = function(dates) {
+    return dates.sort(function(a,b){
+      return new Date(a) - new Date(b)
+    })
+  }
+
+  var getDates = function(records) {
+    var unsortedDates = records.map(function(r) {
+      return r.fields.Date
+    })
+    return sortDates(unsortedDates)
+  }
+
+  var getMeans = function(records) {
+    var moodTable = ["1", "2", "3", "4", "5"]
+    var means = {}
+    records.forEach((function(rec) {
+      var sum_rec = 0
+      var nrec = 0
+      moodTable.forEach(function(moodValue) {
+        nrec += rec.fields[moodValue]
+        sum_rec += parseFloat(moodValue) * rec.fields[moodValue]
+      })
+      means[rec.fields.Date] = sum_rec / parseFloat(nrec)
+    }))
+    return means
+  }
+
+  function range(start, end) {
+    var foo = [];
+    for (var i = start; i <= end; i++) {
+      foo.push(i);
+    }
+    return foo;
+  }
+
+  var trends = function(records) {
+    var means = getMeans(records)
+    var trend = []
+    var dates = getDates(records)
+
+    dates.forEach(function(date) {
+      trend.push(means[date])
+    })
+
+    const spearman = new Spearman(range(0, trend.length - 1), trend)
+
+    return spearman.calc()
+      .then(function(rho) {
+        if (rho < 0) {
+          return 'Bad vibes :disapointed:'
+        } else {
+          return 'OK, good vibes!  :tada:'
+        }
+
+      }).catch(function(err) {console.error(err)})
+  }
+
+  robot.respond(/trends/i, function(res) {
+
+    robot.http("https://api.airtable.com/v0/appDrZAT5gWrRGi6X/NikoNiko?maxRecords=100&filterByFormula=AND(OR(IS_AFTER({Date},'2018-03-01'),IS_SAME({Date},'2018-03-11')),IS_BEFORE({Date},'2018-03-18'))")
+      .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
+      .header('Accept', 'application/json')
+      .get()(function(err, response, body) {
+        var records = JSON.parse(body).records
+        trends(records).then(function(r) {
+          res.reply(r)
+        })
+      })
+  })
+
 }
