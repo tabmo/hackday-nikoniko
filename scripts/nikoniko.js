e4c7906db15144ffb99f74c8928e317a9bd1a42a
Emmanuel Debuire <emmanuel.debuire@tabmo.io>
2018-04-06 16:52:49 +0200

    update event Array on new event

--- scripts/nikoniko.js
+++ scripts/nikoniko.js
@@ -14,19 +14,19 @@ var UtilsHttp = require('./utilsHttp')
 var SubscribersService = require('./subscribersService')
 var StatsUtils = require('./statsUtils')
 
-module.exports = function(robot) {
+module.exports = function (robot) {
 
   var messageSend = false;
   var utilHttp = new UtilsHttp(robot)
   var statsUtils = new StatsUtils()
 
-  setInterval( function() {
+  setInterval(function () {
     var date = new Date();
     if (!messageSend && date.getDay() !== 0 && date.getDay() !== 6 && date.getHours() === 12) {
 
-      utilHttp.getAllSubscribtions(function(err, response, body) {
+      utilHttp.getAllSubscribtions(function (err, response, body) {
         var subscribers = SubscribersService.parseSubscribers(body)
-        subscribers.forEach(function(roomId) {
+        subscribers.forEach(function (roomId) {
           if (roomId.startsWith('D')) { // id of a direct message
             robot.messageRoom(roomId, moodQuestionFile)
           }
@@ -37,32 +37,32 @@ module.exports = function(robot) {
     }
   }, 1000)
 
-  robot.respond(/day/i, function(res) {
+  robot.respond(/day/i, function (res) {
     date = new Date()
     today = date.getFullYear() + "-0" + (date.getMonth() + 1) + "-0" + date.getDate()
-    utilHttp.getMoodLineForDate(today, function(err, response, body) {
+    utilHttp.getMoodLineForDate(today, function (err, response, body) {
       records = JSON.parse(body).records[0]
       total = records.fields["1"] + records.fields["2"] + records.fields["3"] + records.fields["4"] + records.fields["5"]
       chartUrl = "https://image-charts.com/chart?cht=pd&chd=t%3A"
-        + records.fields["1"] +"%2C"
-        + records.fields["2"] +"%2C"
-        + records.fields["3"] +"%2C"
-        + records.fields["4"] +"%2C"
-        + records.fields["5"] +"&chof=.png&chs=800x300&chdl=Pas%20bien%7CBof%7CMoyen%7CBien%7CTr%C3%A8s%20bien&chdls=000000&chco=F56991%2CFF9F80%2CFFC48C%2CD1F2A5%2CEFFAB4&chtt=Humeur%20du%20jour&chdlp=b&chf=bg%2Cs%2CFFFFFF&chbh=10&chli="
+        + records.fields["1"] + "%2C"
+        + records.fields["2"] + "%2C"
+        + records.fields["3"] + "%2C"
+        + records.fields["4"] + "%2C"
+        + records.fields["5"] + "&chof=.png&chs=800x300&chdl=Pas%20bien%7CBof%7CMoyen%7CBien%7CTr%C3%A8s%20bien&chdls=000000&chco=F56991%2CFF9F80%2CFFC48C%2CD1F2A5%2CEFFAB4&chtt=Humeur%20du%20jour&chdlp=b&chf=bg%2Cs%2CFFFFFF&chbh=10&chli="
         + total + "%20Membres&icwt=false"
       res.reply(chartUrl)
     })
 
   });
 
-  robot.respond(/inscrit moi/i, function(conv) {
-    utilHttp.getAllSubscribtions(function(err, response, body) {
+  robot.respond(/inscrit moi/i, function (conv) {
+    utilHttp.getAllSubscribtions(function (err, response, body) {
       var subscribers = SubscribersService.parseSubscribers(body)
       var roomId = conv.message.user.room
 
-      if(!subscribers.includes(roomId)) {
-        var data = JSON.stringify({ fields : { 'Handle': roomId } })
-        utilHttp.addAbonnement(data, function() {
+      if (!subscribers.includes(roomId)) {
+        var data = JSON.stringify({ fields: { 'Handle': roomId } })
+        utilHttp.addAbonnement(data, function () {
           conv.reply("Inscription :white_check_mark: ! Rendez-vous chaque jour à 16h00 !")
         })
       } else {
@@ -71,14 +71,14 @@ module.exports = function(robot) {
     })
   });
 
-  robot.respond(/désinscrit moi/i, function(conv) {
-    utilHttp.getAllSubscribtions(function(err, response, body) {
+  robot.respond(/désinscrit moi/i, function (conv) {
+    utilHttp.getAllSubscribtions(function (err, response, body) {
       var subscribers = SubscribersService.parseSubscribers(body)
       var roomId = conv.message.user.room
-      var subscriber = SubscribersService.getSubscriberByRoomId(body,roomId)
+      var subscriber = SubscribersService.getSubscriberByRoomId(body, roomId)
 
-      if(subscribers.includes(roomId)) {
-        utilHttp.removeAbonnement(subscriber.id, function(){
+      if (subscribers.includes(roomId)) {
+        utilHttp.removeAbonnement(subscriber.id, function () {
           conv.reply("Je ne t'embêterai plus ...  c'est promis ! Si tu changes d'avis, écris moi simplement `inscrit moi` !")
         })
       } else {
@@ -87,60 +87,58 @@ module.exports = function(robot) {
     })
   })
 
-  robot.respond(/Ajoute l'event (.*)/i, function(conv) {
+  robot.respond(/Ajoute l'event (.*)/i, function (conv) {
     var newEvent = conv.match[1]
-    var data = JSON.stringify({ fields : { 'Name': newEvent } })
-    utilHttp.addEvent(data, function() {
+    var data = JSON.stringify({ fields: { 'Name': newEvent } })
+    utilHttp.addEvent(data, function () {
       conv.reply("Event ajouté !")
     })
   })
 
-  robot.respond(/Liste moi les events/i, function(conv) {
-    utilHttp.getAllEvents(function(err, response, body) {
+  robot.respond(/Liste moi les events/i, function (conv) {
+    utilHttp.getAllEvents(function (err, response, body) {
       var records = JSON.parse(body).records
-      var list = records.map(function(s) {
+      var list = records.map(function (s) {
         return s.fields.Name
       })
       conv.reply(list + " Pour ajouter un nouvel évènement à la liste : Ajoute l'event `newEvent`")
     })
   })
 
-  robot.respond(/trends/i, function(res) {
-    utilHttp.getTrendsStats(function(err, response, body) {
+  robot.respond(/trends/i, function (res) {
+    utilHttp.getTrendsStats(function (err, response, body) {
       var records = JSON.parse(body).records
-      statsUtils.trends(records).then(function(r) {
+      statsUtils.trends(records).then(function (r) {
         res.reply(r)
       })
     })
   })
 
 }
-robot.respond(/Aujourd'hui c'est (.*)/i, function(conv) {
+robot.respond(/Aujourd'hui c'est (.*)/i, function (conv) {
   var eventName = conv.match[1]
-  var eventId;  response = robot.http("https://api.airtable.com/v0/appDrZAT5gWrRGi6X/Event%20list?filterByFormula=SEARCH('" +  eventName+ "',event)")
-  .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
-  .header('Accept', 'application/json')
-  .get()(function(err, response, body){
-    eventId = JSON.parse(body).records[0].id
-    console.log('eventId: ', eventId);
-  })
-
-  date = new Date()
-  today = date.getFullYear() + "-0" + (date.getMonth() + 1) + "-0" + date.getDate()
-  response = robot.http("https://api.airtable.com/v0/appDrZAT5gWrRGi6X/NikoNiko?filterByFormula=IS_SAME({Date},'" + today + "')")
-  .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
-  .header('Accept', 'application/json')
-  .get()(function(err, response, body){
-    var id = JSON.parse(body).records[0].id
-    console.log(id);
-    var data = JSON.stringify({ fields : { 'Event': [eventId] }})
-    robot.http('https://api.airtable.com/v0/appDrZAT5gWrRGi6X/NikoNiko/' + id)
-      .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
-      .header('Content-Type', 'application/json')
-      .patch(data)(function(err,response, body){
-        conv.reply(data)
-        console.log("updated")
+  var eventId; response = robot.http("https://api.airtable.com/v0/appDrZAT5gWrRGi6X/Event%20list?filterByFormula=SEARCH('" + eventName + "',event)")
+    .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
+    .header('Accept', 'application/json')
+    .get()(function (err, response, body) {
+      eventId = JSON.parse(body).records[0].id
+      date = new Date()
+      today = date.getFullYear() + "-0" + (date.getMonth() + 1) + "-0" + date.getDate()
+      response = robot.http("https://api.airtable.com/v0/appDrZAT5gWrRGi6X/NikoNiko?filterByFormula=IS_SAME({Date},'" + today + "')")
+        .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
+        .header('Accept', 'application/json')
+        .get()(function (err, response, body) {
+          var dayId = JSON.parse(body).records[0].id
+          var eventsOfDay = JSON.parse(body).records[0].fields.Event
+          eventsOfDay.push(eventId)
+          var data = JSON.stringify({ fields: { 'Event': eventsOfDay } })
+          robot.http('https://api.airtable.com/v0/appDrZAT5gWrRGi6X/NikoNiko/' + dayId)
+            .header('Authorization', 'Bearer keyNUv3Laq95pQOU7')
+            .header('Content-Type', 'application/json')
+            .patch(data)(function (err, response, body) {
+              conv.reply(data)
+              console.log("updated")
+            })
+        })
     })
-  })
-
 })
